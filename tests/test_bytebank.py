e6b21f7b185b69e25c1ae4c078c831db7a5c5574
Esteban Jose Gonzalez Gomez <91167097+Estebanjgg@users.noreply.github.com>
2023-02-20 11:33:29 -0300

    first commit

---
+++ tests/test_bytebank.py
@@ -0,0 +1,54 @@
+from codigo.bytebank import Funcionario
+import pytest
+from pytest import  mark
+
+class TestClass:
+    def test_quando_idade_recebe_13_03_2000_deve_retornar_23(self):
+        entrada = '13/03/2000' # Given-Contexto
+        esperado = 23
+
+        funcionario_teste = Funcionario('Teste', entrada, 1111)
+        resultado = funcionario_teste.idade() # When-ação
+
+        assert resultado == esperado  # Then-desfecho
+
+    def test_quando_sobrenome_recebe_Lucas_Carvalho_deve_retornar_Carvalho(self):
+        entrada = ' Lucas Carvalho' # Give
+        esperado = 'Carvalho'
+
+        lucas = Funcionario(entrada, '11/11/2000', 1111)
+        resultado = lucas.sobrenome() #When
+
+        assert resultado == esperado
+
+
+    def test_quando_decrescimo_salario_recebe_100000_deve_retornar_90000(self):
+        entrada_salario = 100000  # given
+        entrada_nome = 'Paulo Bragança'
+        esperado = 90000
+
+        funcionario_teste = Funcionario(entrada_nome, '11/11/2000', entrada_salario)
+        funcionario_teste.decrescimo_salario()  # when
+        resultado = funcionario_teste.salario
+
+        assert resultado == esperado  # then
+
+    @mark.cacular_bonus
+    def test_quando_calcular_bonus_recebe_1000_deve_retornar_100(self):
+        entrada = 1000  # given
+        esperado = 100
+
+        funcionario_teste = Funcionario('teste', '11/11/2000', entrada)
+        resultado = funcionario_teste.calcular_bonus()  # when
+
+        assert resultado == esperado  # then
+
+    @mark.cacular_bonus
+    def test_quando_calcular_bonus_recebe_100000000_deve_retornar_exception(self):
+        with pytest.raises(Exception):
+            entrada = 100000000  # given
+
+            funconario_teste = Funcionario('teste', '11/11/2000', entrada)
+            resultado = funconario_teste.calcular_bonus()  # when
+
+            assert resultado  # then
